956d54dfad8320f2a4986fefca0d433f19dfcd96
Matt <mvt3641@gmail.com>
2018-03-27 17:57:11 -0400

    changed db port

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,10 @@ var mysql=require('mysql');
 var connection =mysql.createConnection({
 
   host: 'localhost',
-  port:  3306,
+  port:  3000,
 
   user: 'root',
-  password: 'root',
+  password: 'DodgeCity41',
   database: 'flight_data_db'
 });
 
