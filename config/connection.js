b65fab5b3ebfd74e4461937781b14fce5015b3ef
Matt <mvt3641@gmail.com>
2018-10-13 19:55:59 -0400

    updating

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ var mongoose =require('mongoose');
 mongoose.Promise = global.Promise;
 var dbc;
 // mongoose.Promise = Promise;
-var connection = mongoose.connect("mongodb://localhost/")
+// var connection = mongoose.connect("mongodb://localhost/")
+var connection = mongoose.connect("mongodb://flighttest:coding41@ds231723.mlab.com:31723/flightdata_testdb")
 .then(function(){
 console.log("database connected..");
 });
